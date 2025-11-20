ac12dfa2e2cc1dd28a2c935227f35d75f8d5eb83
ROT <rohit93dgrr@gmail.com>
2025-11-20 18:00:52 +0000

    https://cliply.co/tag/bigmoji-collection/ ,https://gifgifs.com/anime/ ,h

--- src/lib/sticker-gif-data.ts
+++ src/lib/sticker-gif-data.ts
@@ -1,24 +1,31 @@
 
 export const stickerGifData = {
     stickers: [
-        { id: "s1", url: "https://media.flaticon.com/dist/min/img/stickers/graphic-design/4.svg", alt: "Like sticker"},
-        { id: "s2", url: "https://media.flaticon.com/dist/min/img/stickers/graphic-design/5.svg", alt: "Love it sticker"},
-        { id: "s3", url: "https://media.flaticon.com/dist/min/img/stickers/graphic-design/6.svg", alt: "Idea sticker"},
-        { id: "s4", url: "https://media.flaticon.com/dist/min/img/stickers/graphic-design/11.svg", alt: "Star sticker"},
-        { id: "s5", url: "https://media.flaticon.com/dist/min/img/stickers/startup/24.svg", alt: "Thinking sticker"},
-        { id: "s6", url: "https://media.flaticon.com/dist/min/img/stickers/startup/13.svg", alt: "Success sticker"},
-        { id: "s7", url: "https://media.flaticon.com/dist/min/img/stickers/startup/11.svg", alt: "Rocket launch sticker"},
-        { id: "s8", url: "https://media.flaticon.com/dist/min/img/stickers/startup/23.svg", alt: "Confused sticker"},
+        { id: "s1", url: "https://cdn.discordapp.com/emojis/860601963244224532.webp?size=96&quality=lossless", alt: "Thumbs Up"},
+        { id: "s2", url: "https://cdn.discordapp.com/emojis/750543662283030529.webp?size=96&quality=lossless", alt: "Heart"},
+        { id: "s3", url: "https://cdn.discordapp.com/emojis/860601962908090380.webp?size=96&quality=lossless", alt: "Laughing"},
+        { id: "s4", url: "https://cdn.discordapp.com/emojis/860601963071340574.webp?size=96&quality=lossless", alt: "Waving"},
+        { id: "s5", url: "https://cdn.discordapp.com/emojis/860601963345477652.webp?size=96&quality=lossless", alt: "Mind Blown"},
+        { id: "s6", url: "https://cdn.discordapp.com/emojis/860601963386634280.webp?size=96&quality=lossless", alt: "Cool"},
+        { id: "s7", url: "https://cdn.discordapp.com/emojis/750543662123597835.webp?size=96&quality=lossless", alt: "Sad"},
+        { id: "s8", url: "https://cdn.discordapp.com/emojis/860601963032510524.webp?size=96&quality=lossless", alt: "Angry"},
+        { id: "s9", url: "https://cdn.discordapp.com/emojis/750543662174093312.webp?size=96&quality=lossless", alt: "Thinking"},
+        { id: "s10", url: "https://cdn.discordapp.com/emojis/750543662253801523.webp?size=96&quality=lossless", alt: "Celebrate"},
+        { id: "s11", url: "https://cdn.discordapp.com/emojis/750543662362722304.webp?size=96&quality=lossless", alt: "Hugging"},
+        { id: "s12", url: "https://cdn.discordapp.com/emojis/860601963300847636.webp?size=96&quality=lossless", alt: "Sleeping"}
     ],
     gifs: [
-        { id: "g1", url: "https://media.pixabay.com/media/u8r3u9Kij02sNo2c36/giphy.gif", alt: "Well done GIF"},
-        { id: "g2", url: "https://media.pixabay.com/media/wZAdlPQLY7lAbDNoY7/giphy.gif", alt: "Good job GIF"},
-        { id: "g3", url: "https://media.pixabay.com/media/OUJJYf9lTUFb5kvy6s/giphy.gif", alt: "Excellent GIF"},
-        { id: "g4", url: "https://media.pixabay.com/media/kAFMNIh9qfiiU4x091/giphy.gif", alt: "Clapping GIF"},
-        { id: "g5", url: "https://media.pixabay.com/media/xT0xez7Z2rNzr2Bpr2/giphy.gif", alt: "Thinking GIF"},
-        { id: "g6", url: "https://media.pixabay.com/media/l41lI42A2j2Ef9bPO/giphy.gif", alt: "Thumbs up GIF"},
-        { id: "g7", url: "https://media.pixabay.com/media/J2rfpWf5HqViYJ2SQf/giphy.gif", alt: "On my way GIF"},
-        { id: "g8", url: "https://media.pixabay.com/media/Hw8vYF4DN1a2sNo2Bpr2B/giphy.gif", alt: "Deal with it GIF"},
-        { id: "g9", url: "https://i.gifer.com/742.gif", alt: "Good luck GIF"},
+        { id: "g1", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTZyM3V6OXcxMm11cHlnajJ4NWVqNWlpbHNoaXRmdzN2Yjh2cjI1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hZj44bR9FVI3K/giphy.gif", alt: "Anime GIF"},
+        { id: "g2", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWNnMnZzMGI2dGFxOHl1ZXVxZWw4cXd0enRkMnVudHU2NG1xM3RmeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Lqxp2iVd1mJmI2mZ3t/giphy.gif", alt: "Anime GIF"},
+        { id: "g3", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTV2c2U5eHN0dDZ0Nmk2aDN0OHB0anJua25zMnE4dHVudnN3cGw3bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5s1Ig0s3Q5y3b3A0y2/giphy.gif", alt: "What GIF"},
+        { id: "g4", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXA0OHRjZDE2ZmFocXkxY3ZxcTE1OXJ0OHhva2VtdzV2cWdxejNuaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1X7lCRpI2I3w4/giphy.gif", alt: "Anime GIF"},
+        { id: "g5", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYTc2eHZucnB5ZzF3cjNqOGZzYmJzMXM0eHhpbzZtcDB2cTl2dzB2MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/y3x9r1nOLaiY/giphy.gif", alt: "Typing GIF"},
+        { id: "g6", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWY0Z3p0M2dwZnd0c3VtcTI0OHJzdmY5enZlMHEzeGl5N2YxNmxqbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TpsuCxwsNH8gatbp3v/giphy.gif", alt: "Okay GIF"},
+        { id: "g7", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHF4YzJ0MGU1eWtlNWhjNXZ0emN5dGRsYTVtOGs0NDVzOGN5cTdrdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPf3C7HqqYbvdeM/giphy.gif", alt: "Good Morning GIF"},
+        { id: "g8", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTJndnI4MmN3NWd3MjVwM3N0cGtoZnVmMmQ4cW1wMG5kM2Yzb3YxNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eNTxLwTGW7E64/giphy.gif", alt: "Anime GIF"},
+        { id: "g9", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGZyNHNvYzhpNTQ1ZXdkNmVtdXJkNWJvbWQ1MG5idndrbG85NWc4dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1d5ZNzBD423mg/giphy.gif", alt: "Anime GIF"},
+        { id: "g10", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDVtcXZmdnBpaDk5ajg0amZvcXo2OWJva3B2anJ5eWZtcWJ5NGh0ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RgzryV9PyTVeM/giphy.gif", alt: "Laughing GIF"},
+        { id: "g11", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnIzNDR0cm1hMm51aHRxNHNtdzVqZWt3cDI2eWNla3oxM3JnaDdiOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4Z3Vbs6e8W08t6maS8/giphy.gif", alt: "Yes GIF"},
+        { id: "g12", url: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHFicXljbTZzYmFuc2Q5ZXQ1eHZ2b2hjeXo1MnVudG15NHU1Nnc2ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MDJ9IbxxvDUQM/giphy.gif", alt: "Anime GIF"}
     ]
 }
