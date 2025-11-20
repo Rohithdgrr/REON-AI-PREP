be1a262f6b61108adfbde9db9995b69d9d0f4fd2
ROT <rohit93dgrr@gmail.com>
2025-11-20 17:33:25 +0000

    sort this error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6821515882-cca40",
   "appId": "1:641214223552:web:c0631590e752e01c044925",
+  "storageBucket": "studio-6821515882-cca40.appspot.com",
   "apiKey": "AIzaSyAPARJD5xPtBVdOLoqsxOXj8fkObc_jh2g",
   "authDomain": "studio-6821515882-cca40.firebaseapp.com",
   "measurementId": "",
