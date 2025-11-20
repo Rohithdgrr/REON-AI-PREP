25c4c0514090e4c7087e93842f2dd6d8891be72b
ROT <rohit93dgrr@gmail.com>
2025-11-20 16:02:00 +0000

    how to change this name studio-68xxxxxxxxxx-firebaseapp.com to my websit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-6821515882-cca40",
   "appId": "1:641214223552:web:c0631590e752e01c044925",
   "apiKey": "AIzaSyAPARJD5xPtBVdOLoqsxOXj8fkObc_jh2g",
-  "authDomain": "studio-6821515882-cca40.firebaseapp.com",
+  "authDomain": "your-custom-domain.com",
   "measurementId": "",
   "messagingSenderId": "641214223552"
 };
