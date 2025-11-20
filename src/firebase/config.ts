bd9a62ff4fece68d935a08d0789453d0ab6869e6
ROT <rohit93dgrr@gmail.com>
2025-11-20 16:04:39 +0000

    revert to old domain name as of now.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-6821515882-cca40",
   "appId": "1:641214223552:web:c0631590e752e01c044925",
   "apiKey": "AIzaSyAPARJD5xPtBVdOLoqsxOXj8fkObc_jh2g",
-  "authDomain": "your-custom-domain.com",
+  "authDomain": "studio-6821515882-cca40.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "641214223552"
 };
