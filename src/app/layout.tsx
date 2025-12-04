857de310c6ef2efe5777116f3bb3089fabbd15e2
ROT <rohit93dgrr@gmail.com>
2025-12-04 17:16:19 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { ToolsSidebarProvider } from '@/hooks/use-tools-sidebar';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase';
-import { ClientWrapper } from '@/components/client-wrapper';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-body' });
 const lexend = Lexend({ subsets: ['latin'], variable: '--font-headline' });
@@ -26,8 +25,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body 
         className={cn('min-h-screen bg-background font-body antialiased', inter.variable, lexend.variable)}
+        suppressHydrationWarning={true}
       >
-        <ClientWrapper>
           <FirebaseClientProvider>
             <ThemeProvider
               attribute="class"
@@ -42,7 +41,6 @@ export default function RootLayout({
               </ToolsSidebarProvider>
             </ThemeProvider>
           </FirebaseClientProvider>
-        </ClientWrapper>
       </body>
     </html>
   );
